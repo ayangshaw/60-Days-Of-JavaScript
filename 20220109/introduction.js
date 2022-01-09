dd7ab9a15dee395f6c787b96a7dcafcf770f2b65
ayangshaw <ayang.shaw@gmail.com>
2022-01-09 23:33:10 +0800

    practice 20220109 shawn

---
+++ 20220109/introduction.js
@@ -0,0 +1,20 @@
+console.log('60-days-of-javascript-study!')
+console.log('hello world!')
+
+let str = 'shawn';
+let num = 0;
+let isApproved = true;
+let und = undefined;
+let nothing = null;
+let type1 = typeof str;
+let type2 = typeof num;
+let type3 = typeof isApproved;
+let type4 = typeof und;
+let type5 = typeof nothing;
+
+console.log(type1, type2, type3, type4, type5)
+// single line comment;
+
+/*
+ multiline comment
+*/
